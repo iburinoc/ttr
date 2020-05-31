842785c9ccc1a941dd52fd3eb321aa5f22c46b55
Sean Purcell <me@seanp.xyz>
2020-05-31 14:54:34 -0400

    Set up basic registrator

---
+++ examples/browse.rs
@@ -0,0 +1,138 @@
+use async_dnssd::{ResolvedHostFlags, StreamTimeoutExt, TxtRecord};
+use futures::prelude::*;
+use std::{env, time::Duration};
+use tokio::spawn;
+
+#[tokio::main(basic_scheduler)]
+async fn main() {
+    let search_timeout = Duration::from_secs(10);
+    let resolve_timeout = Duration::from_secs(3);
+    let address_timeout = Duration::from_secs(3);
+
+    // MetaQuery to list all services available.
+    let list_all_services = "_services._dns-sd._udp";
+
+    // Use `cargo run --example browse` to list all services broadcasting
+    // or `cargo run --example browse -- _http._tcp` to resolve a service.
+    let query = &env::args()
+        .nth(1)
+        .unwrap_or_else(|| list_all_services.to_string());
+    println!("Browse: {}", query);
+
+    let query_result = async_dnssd::browse(&query)
+        .expect("failed browse")
+        .timeout(search_timeout)
+        .expect("failed timeout");
+    query_result
+        .try_for_each(move |service| {
+            async move {
+                let added = service.flags.contains(async_dnssd::BrowsedFlags::ADD);
+                if query == list_all_services {
+                    // resolving MetaQuery responses isn't useful (and fails
+                    // with "bad param")... we'd need to browse them
+
+                    let mut reg_type = service.reg_type.as_str();
+                    if reg_type.ends_with('.') {
+                        reg_type = &reg_type[..reg_type.len() - 1];
+                    }
+                    let reg_type = service.service_name.clone() + "." + reg_type;
+                    println!(
+                        "Service Type {}{:?}@{:?}\t\t[{:?}]",
+                        if added { '+' } else { '-' },
+                        reg_type,
+                        service.domain,
+                        service
+                    );
+
+                    return Ok(());
+                }
+
+                println!(
+                    "Service {}{:?}@{:?} (type {:?})\t\t[{:?}]",
+                    if added { '+' } else { '-' },
+                    service.service_name,
+                    service.domain,
+                    service.reg_type,
+                    service
+                );
+
+                if !added {
+                    // only resolve added services
+                    return Ok(());
+                }
+
+                spawn(async move {
+                    let resolve = match service.resolve() {
+                        Ok(r) => r,
+                        Err(e) => {
+                            println!("resolve failed: {:?}", e);
+                            return;
+                        }
+                    }
+                    .timeout(resolve_timeout)
+                    .expect("failed timeout");
+                    let service = &service;
+                    if let Err(e) = resolve
+                        .try_for_each(move |r| {
+                            async move {
+                                let txt = TxtRecord::parse(&r.txt).map(|rdata| {
+                                    rdata
+                                        .iter()
+                                        .map(|(key, value)| {
+                                            (
+                                                String::from(String::from_utf8_lossy(key)),
+                                                value.map(|value| {
+                                                    String::from(String::from_utf8_lossy(value))
+                                                }),
+                                            )
+                                        })
+                                        .collect::<Vec<_>>()
+                                });
+                                println!(
+                                    "Resolved {:?} on {:?}: {:?}:{} (txt {:?})\t\t[{:?}]",
+                                    service.service_name,
+                                    r.interface,
+                                    r.host_target,
+                                    r.port,
+                                    txt,
+                                    r
+                                );
+                                let fullname = r.fullname.clone();
+                                let host_target_e = r.host_target.clone();
+                                spawn(
+                                    // Query IPv4 + IPv6
+                                    r.resolve_socket_address()?
+                                        .timeout(address_timeout)?
+                                        .try_for_each(move |result| {
+                                            if result.flags.intersects(ResolvedHostFlags::ADD) {
+                                                println!(
+                                                    "Address for {}: {}",
+                                                    fullname, result.address
+                                                );
+                                            }
+                                            futures::future::ok(())
+                                        })
+                                        .map(move |r| {
+                                            if let Err(e) = r {
+                                                println!(
+                                                    "query_record {} failed: {}",
+                                                    host_target_e, e
+                                                );
+                                            }
+                                        }),
+                                );
+                                Ok(())
+                            }
+                        })
+                        .await
+                    {
+                        println!("resolve {:?} failed: {}", service.service_name, e);
+                    }
+                });
+
+                Ok(())
+            }
+        })
+        .await
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,44 @@
-use std::net::Ipv4Addr;
+use std::{env, time::Duration};
 
-use net2::UdpBuilder;
-use structopt::StructOpt;
-use tokio::net::UdpSocket;
+use async_dnssd::{RegisterData, ResolvedHostFlags, StreamTimeoutExt, TxtRecord};
+use futures::prelude::*;
+use tokio::spawn;
+use uuid::Uuid;
 
-#[derive(StructOpt)]
-struct Opt {
-    #[structopt(short, long, default_value = "5000")]
-    port: u16,
-
-    #[structopt(short, long, default_value = "127.0.0.1")]
-    host: String,
-
-    #[structopt(short, long)]
-    target: Option<String>,
-
-    #[structopt(short, long)]
-    msg: Option<String>,
-}
-
-#[tokio::main]
+#[tokio::main(basic_scheduler)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = Opt::from_args();
-    let mut listener = UdpSocket::from_std(
-        UdpBuilder::new_v4()?
-            .reuse_address(true)?
-            .bind((opts.host.as_str(), opts.port))?,
-    )?;
+    let search_timeout = Duration::from_secs(10);
+    let resolve_timeout = Duration::from_secs(3);
+    let address_timeout = Duration::from_secs(3);
 
-    listener.join_multicast_v4("224.0.0.251".parse()?, "192.168.2.45".parse()?)?;
-
-    println!("Listening on {:?}", listener.local_addr()?);
-
-    if let Some(msg) = opts.msg {
-        if let Some(target) = opts.target {
-            let bytes = msg.as_bytes();
-            listener.connect(target).await?;
-            listener.send(bytes).await?;
-        }
-    }
-
-    let mut buf = [0u8; 2048];
+    let uuid = Uuid::new_v4().to_hyphenated().to_string();
+    let mut record = TxtRecord::new();
+    record
+        .set_value("gameStatus".as_bytes(), "1".as_bytes())
+        .unwrap();
+    record
+        .set_value("platform".as_bytes(), "generic".as_bytes())
+        .unwrap();
+    record
+        .set_value("uuid".as_bytes(), uuid.as_bytes())
+        .unwrap();
+    record
+        .set_value("version".as_bytes(), "2.7.6".as_bytes())
+        .unwrap();
+    record
+        .set_value("_d".as_bytes(), "sean".as_bytes())
+        .unwrap();
+    let _register = async_dnssd::register_extended(
+        "_t2rdaysofwonder._tcp",
+        64444,
+        RegisterData {
+            txt: record.data(),
+            ..Default::default()
+        },
+    )?
+    .await?;
 
     loop {
-        let (amt, from) = listener.recv_from(&mut buf).await?;
-        println!("Recv {:?}: {:02x?}", from, &buf[..amt]);
+        tokio::time::delay_for(Duration::from_secs(10)).await;
     }
 }
