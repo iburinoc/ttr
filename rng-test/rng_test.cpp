3139165101bcbe2969c8e68a64a375d13235135d
Sean Purcell <me@seanp.xyz>
2020-06-14 00:55:56 -0400

    Overhaul randomization testing

--- rng-test/rng_test.cpp
+++ rng-test/rng_test.cpp
@@ -3,21 +3,71 @@ using namespace std;
 
 int main(int argc, char **argv) {
   uint32_t seed = (uint32_t)stoull(argv[1]);
-  minstd_rand g(seed);
+  minstd_rand gen(seed);
 
-  for (int i = 0; i < 200; i++) {
-    auto gen = g;
-    gen.discard(i);
-    vector<int> v(110);
-    iota(v.begin(), v.end(), 0);
-    cout << i << ": ";
-    for (int j = 0; j < 13; j++) {
+  vector<int> tickets(46);
+  iota(tickets.begin(), tickets.end(), 0);
+  vector<int> bigs;
+
+  auto erase = [&](int i) {
+    tickets.erase(find(tickets.begin(), tickets.end(), i));
+    bigs.push_back(i);
+  };
+
+  erase(11);
+  erase(15);
+  erase(16);
+  erase(21);
+  erase(24);
+  erase(31);
+
+  auto shuffle1 = [&](vector<int> v) {
+    for (int i = 0; i < v.size() - 1; i++) {
+      int idx = uniform_int_distribution<uint_fast32_t>(i, v.size() - 1)(gen);
+      swap(v[idx], v[i]);
+    }
+    return v;
+  };
+  auto shuffle2 = [&](vector<int> v) {
+    vector<int> o;
+    while (v.size() > 1) {
       int idx = uniform_int_distribution<uint_fast32_t>(0, v.size() - 1)(gen);
-      auto iter = v.begin() + idx;
-      int val = *iter;
-      v.erase(iter);
-      cout << setw(3) << val << " ";
+      o.push_back(v[idx]);
+      v.erase(v.begin() + idx);
+    }
+    o.push_back(v[0]);
+    return o;
+  };
+
+  tickets = shuffle2(tickets);
+  bigs = shuffle2(bigs);
+
+  for (int i = 0; i < 6; i++) {
+    cout << tickets[i] << " ";
+  }
+  cout << endl;
+  for (int i = 0; i < 2; i++) {
+    cout << bigs[i] << " ";
+  }
+  cout << endl;
+
+  vector<int> trains(110);
+  iota(trains.begin(), trains.end(), 0);
+  auto pick = [&]() {
+    int idx =
+        uniform_int_distribution<uint_fast32_t>(0, trains.size() - 1)(gen);
+    auto iter = trains.begin() + idx;
+    int val = *iter;
+    trains.erase(iter);
+    return val;
+  };
+  auto pickN = [&](int n) {
+    for (int j = 0; j < n; j++) {
+      cout << setw(3) << pick() << " ";
     }
     cout << endl;
-  }
+  };
+  pickN(5);
+  pickN(4);
+  pickN(4);
 }
